9e7196231eb6a016cfeb398d236f4e1bdd74a1cf
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2024-01-25 21:54:18 +0300

    started on pygame

--- client.py
+++ client.py
@@ -1,15 +1,30 @@
 import socket
 import pygame
 
+def main():
+    screen = pygame.display.set_mode((W, H))
+    screen.fill((0, 0, 0))
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 sock.connect(('', 1092))
 
+pygame.init()
+W, H = 32 * 13 + 120, 32 * 13 + 120
+clock = pygame.time.Clock()
+
 while True:
-    sock.send("Some command".encode())
+    #sock.send("Some command".encode())
 
-    data = sock.recv(2**20)
-    data = data.decode()
-
+    data = sock.recv(2**20).decode()
+    #data = data.decode()
     print(data)
+    if data == "start":
+        do = True
+        break
+
+
+while do:
+    main()
+pygame.quit()
--- server.py
+++ server.py
@@ -1,41 +1,70 @@
 import socket
 import time
 
+class Player:
+    def __init__(self, sock):
+        self.sock = sock
+        self.errors = 0
+
+def send_data_to_both_players(data):
+    for player in players:
+        try:
+            player.sock.send(data.encode())
+            player.errors = 0
+        except:
+            if player.errors >= 10:
+                players.remove(player.sock)
+                player.sock.close()
+                print("Player disconnected")
+                end_game("one player disconnected")
+
+def end_game(msg):
+    #sth to end the game
+    print("GAME ENDED: ", msg)
+
+
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 main_socket.bind(('', 1092))
 main_socket.setblocking(0)
 main_socket.listen(2)
 
-players_sockets = []
+# making connection with 2 players
+players = []
 while True:
-    # connect players
-    try:
+    try:  # connect players
         new_socket, addr = main_socket.accept()
         print("Connected ", addr)
         new_socket.setblocking(0)
-        players_sockets.append(new_socket)
+        players.append(Player(new_socket))
+        if len(players) == 2:
+            break
     except:
         print("nothing")
+    time.sleep(0.1)
 
+#ready to start the game
+send_data_to_both_players("start")
+
+
+while True:
     # read players' commands
-    for sock in players_sockets:
+    for player in players:
         try:
-            data = sock.recv(1024)
+            data = player.setblocking.recv(1024)
             data = data.decode()
             print("Server got ", data)
         except:
             pass
-
     # parse commands
 
     # send new data to players
-    for sock in players_sockets:
+    for player in players:
         try:
-            sock.send("New data".encode())
+            player.sock.send("New data".encode())
         except:
-            players_sockets.remove(sock)
-            sock.close()
+            players.remove(player.sock)
+            player.sock.close()
             print("Player disconnected")
 
     time.sleep(50)
