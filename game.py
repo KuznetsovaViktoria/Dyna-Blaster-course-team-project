8e5e1a8a1140d80f105966acb6a51404f976a43c
Erika Pastukhova <pastukhova.erika@gmail.com>
2024-02-14 16:33:26 +0300

    game realisation but sometimes tank cannot go between blocks

--- game.py
+++ game.py
@@ -1,32 +1,166 @@
-# START WORKING
 import pygame
-import sys
-from pygame.color import THECOLORS
+from random import randint
 
-pygame.init()  # prepare modules of pygame to work
+pygame.init()
 
-screen = pygame.display.set_mode((1200, 800))  # create graphics window, (1200, 800) - resolution, -> Surface object for images
-list_of_fonts = pygame.font.get_fonts()
-# print(*list_of_fonts)
-font = pygame.font.SysFont(list_of_fonts[10], 40)
-text = font.render(str('Game by Erika and Vika'), True, THECOLORS['purple'])
-screen.fill(THECOLORS['white'])  # make all screen white
-screen.blit(text, (350, 50))
-r = pygame.Rect(400, 300, 400, 300)  # coordinates: upper left corner, length of sides
-                                 # coordinate (0, 0) - upper left corner of screen
-pygame.draw.rect(screen, (255, 0, 255), r, 0)  # width = 0 -> full colored
-# pygame.draw.line(screen, (0, 255, 0), (0, 0), (100, 100), width = 10)
-# pygame.draw.line(screen, THECOLORS['orange'], (100, 100), (0, 200), width = 10)
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+TILE = 50
 
-while True:
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+DIRECTS = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+
+
+class Tank:
+    def __init__(self, color, px, py, direct, keyList):
+        objects.append(self)
+        self.type = 'tank'
+
+        self.color = color
+        self.rect = pygame.Rect(px, py, TILE, TILE)
+        self.direct = direct
+        self.moveSpeed = 2
+        self.hp = 5
+
+        self.shotTimer = 0
+        self.shotDelay = 60
+        self.bulletSpeed = 5
+        self.bulletDamage = 1
+
+        self.keyLEFT = keyList[0]
+        self.keyRIGHT = keyList[1]
+        self.keyUP = keyList[2]
+        self.keyDOWN = keyList[3]
+        self.keySHOT = keyList[4]
+
+    def update(self):
+        oldX, oldY = self.rect.topleft
+        if keys[self.keyLEFT]:
+            self.rect.x -= self.moveSpeed
+            self.direct = 3
+        elif keys[self.keyRIGHT]:
+            self.rect.x += self.moveSpeed
+            self.direct = 1
+        elif keys[self.keyUP]:
+            self.rect.y -= self.moveSpeed
+            self.direct = 0
+        elif keys[self.keyDOWN]:
+            self.rect.y += self.moveSpeed
+            self.direct = 2
+
+        for obj in objects:
+            if obj != self and self.rect.colliderect(obj.rect):
+                self.rect.topleft = oldX, oldY
+
+        if keys[self.keySHOT] and self.shotTimer == 0:
+            dx = DIRECTS[self.direct][0] * self.bulletSpeed
+            dy = DIRECTS[self.direct][1] * self.bulletSpeed
+            Bullet(self, self.rect.centerx, self.rect.centery, dx, dy, self.bulletDamage)
+            self.shotTimer = self.shotDelay
+
+        if self.shotTimer > 0: self.shotTimer -= 1
+
+    def draw(self):
+        pygame.draw.rect(window, self.color, self.rect)
+
+        x = self.rect.centerx + DIRECTS[self.direct][0] * 30
+        y = self.rect.centery + DIRECTS[self.direct][1] * 30
+        pygame.draw.line(window, 'white', self.rect.center, (x, y), 4)
+
+    def damage(self, value):
+        self.hp -= value
+        if self.hp <= 0:
+            objects.remove(self)
+            print(self.color, 'dead')
+
+
+class Bullet:
+    def __init__(self, parent, px, py, dx, dy, damage):
+        bullets.append(self)
+        self.parent = parent
+        self.px, self.py = px, py
+        self.dx, self.dy = dx, dy
+        self.damage = damage
+
+    def update(self):
+        self.px += self.dx
+        self.py += self.dy
+
+        if self.px < 0 or self.px > WIDTH or self.py < 0 or self.py > HEIGHT:
+            bullets.remove(self)
+        else:
+            for obj in objects:
+                if obj != self.parent and obj.rect.collidepoint(self.px, self.py):
+                    obj.damage(self.damage)
+                    bullets.remove(self)
+                    break
+
+    def draw(self):
+        pygame.draw.circle(window, 'yellow', (self.px, self.py), 2)
+
+
+class Block:
+    def __init__(self, px, py, size):
+        objects.append(self)
+        self.type = 'block'
+
+        self.rect = pygame.Rect(px, py, size, size)
+        self.hp = 1
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pygame.draw.rect(window, 'green', self.rect)
+        pygame.draw.rect(window, 'gray20', self.rect, 2)
+
+    def damage(self, value):
+        self.hp -= value
+        if self.hp <= 0: objects.remove(self)
+
+
+bullets = []
+objects = []
+Tank('blue', 100, 275, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE))
+Tank('red', 650, 275, 0, (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_0))
+
+for _ in range(50):
+    while True:
+        x = randint(0, WIDTH // TILE - 1) * TILE
+        y = randint(0, HEIGHT // TILE - 1) * TILE
+        rect = pygame.Rect(x, y, TILE, TILE)
+        ok = True # не рисуем блок на другом объекте
+        for obj in objects:
+            if rect.colliderect(obj.rect):
+                ok = False
+
+        if ok:
+            break
+
+    Block(x, y, TILE)
+
+play = True
+while play:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # if escape
-            pygame.quit()  # end work with pygame
-            sys.exit()  # end programm
-    pygame.display.flip()  # changes to be visable
+        if event.type == pygame.QUIT:
+            play = False
 
+    keys = pygame.key.get_pressed()
 
-# DRAW GEOM FIGURES
-# Rect(left, top, width, height)  # make a rectangle, type Rect for such objects
-# Rect((left, top), (width, height))
-# rect(Surface, color, Rect, width = 0) -> Rect  # in pygame dict to draw geom. figures - module draw. rect() draws rectangle
+    for bullet in bullets:
+        bullet.update()
+    for obj in objects:
+        obj.update()
+
+    window.fill('black')
+    for bullet in bullets:
+        bullet.draw()
+    for obj in objects:
+        obj.draw()
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
